426efe111ba939a265bd66ab406b58b5eb210022
Marquis Kurt <software@marquiskurt.net>
2024-01-27 11:44:34 -0500

    Revert to original movement code

--- charolette/movement.c
+++ charolette/movement.c
@@ -5,7 +5,7 @@
 vec2f get_translated_movement(vec2f original, float crankAngle, vec2f bounds) {
     // TODO: Correctly calculate the delta here!
     float delta = original.x - crankAngle;
-    struct vec2f newVector = {original.x + delta, original.y};
+    struct vec2f newVector = {crankAngle, original.y};
     if (newVector.x > bounds.x)
         newVector.x = 0.0f;
     if (newVector.y > bounds.y)
