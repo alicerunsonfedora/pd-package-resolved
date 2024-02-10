c97c80d0f844fd3c926dc4d00296a78a4f441807
Marquis Kurt <software@marquiskurt.net>
2024-02-09 22:36:12 -0500

    Use sprites for boxes instead of a

--- src/main.c
+++ src/main.c
@@ -47,14 +47,21 @@ int eventHandler(PlaydateAPI *pd, PDSystemEvent event, uint32_t arg) {
 const inset walls = {0, X_INSET, X_INSET, 0};
 
 int frame = 0;
+int boxframe = 0;
 bool frameUpdated = false;
 bool initializedGameLoop = false;
 vec2f spritePosition = {0.0f, 24.0f};
 vec2i spriteSize = {CHARLIE_WIDTH, CHARLIE_HEIGHT};
 vec2f screenBounds = {0.0f, 0.0f};
 LCDBitmapTable *table;
+LCDBitmapTable *boxTable;
+
 LCDBitmap *spriteImage;
 LCDSprite *sprite;
+
+LCDBitmap *boxOnFrame;
+LCDBitmap *boxOffFrame;
+
 vec2f boxes[BOXES_COUNT];
 int boxesCollected = 0;
 char *counterMessage;
@@ -68,6 +75,24 @@ void cycle() {
     if (frame > 5)
         frame = 0;
     frameUpdated = true;
+}
+
+void drawBox(int index, PlaydateAPI *pd) {
+    vec2f box = boxes[index];
+    bool even = index % 2 == 0;
+    if (boxframe == 1) {
+        if (even) {
+            pd->graphics->drawBitmap(boxOnFrame, box.x, box.y, kBitmapUnflipped);
+        } else {
+            pd->graphics->drawBitmap(boxOffFrame, box.x, box.y, kBitmapUnflipped);
+        }
+    } else {
+        if (!even) {
+            pd->graphics->drawBitmap(boxOnFrame, box.x, box.y, kBitmapUnflipped);
+        } else {
+            pd->graphics->drawBitmap(boxOffFrame, box.x, box.y, kBitmapUnflipped);
+        }
+    }
 }
 
 static int update(void *userdata) {
@@ -88,6 +113,17 @@ static int update(void *userdata) {
             return 0;
         }
         spriteImage = pd->graphics->getTableBitmap(table, frame);
+        
+        // Box setup
+        const char *boxsheet = "Images/box";
+        boxTable = loadTable(boxsheet, pd);
+        if (boxTable == NULL) {
+            pd->system->error("The table for path %s is missing.", boxsheet);
+            return 0;
+        }
+        
+        boxOnFrame = pd->graphics->getTableBitmap(boxTable, 0);
+        boxOffFrame = pd->graphics->getTableBitmap(boxTable, 1);
 
         if (spriteImage != NULL && sprite == NULL) {
             sprite = imagedSprite(pd, spriteSize, spriteImage);
@@ -98,6 +134,7 @@ static int update(void *userdata) {
         pd->sprite->updateAndDrawSprites();
 
         fill_boxes(boxes, 6, screenBounds, walls);
+        counterMessage = "Boxes collected: 0";
 
         initializedGameLoop = true;
         return 1;
@@ -110,13 +147,11 @@ static int update(void *userdata) {
     pd->sprite->markDirty(sprite);
     pd->sprite->updateAndDrawSprites();
 
-    pd->system->formatString(&counterMessage, "Boxes collected: %i", boxesCollected);
-    pd->graphics->drawText(counterMessage, strlen(counterMessage), kASCIIEncoding, 8,
-                           screenBounds.y - 14 - 8);
-
     // Boxes
+    int currentBoxesCollectedInFrame = boxesCollected;
+    boxframe = (frame > 2) ? 1 : 0;
     for (int i = 0; i < BOXES_COUNT; i++) {
-        pd->graphics->drawText("a", strlen("a"), kASCIIEncoding, boxes[i].x, boxes[i].y);
+        drawBox(i, pd);
         boxes[i] =
             shift_box(boxes[i], -CHARLIE_HEIGHT, i, screenBounds, BOXES_COUNT, walls);
 
@@ -126,6 +161,11 @@ static int update(void *userdata) {
             boxesCollected++;
         }
     }
+    
+    if (boxesCollected > currentBoxesCollectedInFrame)
+        pd->system->formatString(&counterMessage, "Boxes collected: %i", boxesCollected);
+    pd->graphics->drawText(counterMessage, strlen(counterMessage), kASCIIEncoding, 8,
+       screenBounds.y - 14 - 8);
 
     // Actions
     cycle();
