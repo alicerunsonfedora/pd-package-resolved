f6108c3a1439498b6b3f312591bf0ba334140917
Marquis Kurt <software@marquiskurt.net>
2024-02-12 10:42:47 -0500

    Add more documentation comments

--- src/text.h
+++ src/text.h
@@ -4,6 +4,13 @@
 #include "pd_api.h"
 #include "vector.h"
 
+/**
+ * Draws a string onto the screen using the ASCII encoding.
+ *
+ * @param pd        The Playdate API object that will draw text to the screen.
+ * @param text      The text to be rendered onto the screen.
+ * @param position  The position that the text should be rendered ad.
+ */
 void drawASCIIText(PlaydateAPI *pd, const char *text, vec2i position);
 
 #endif
