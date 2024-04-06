8c0f946219a9600c262c0d7477af80bf413decc4
Marquis Kurt <software@marquiskurt.net>
2024-04-06 15:38:19 -0400

    Provide docs for Screen APIs

--- Sources/Charolette/Screen.swift
+++ Sources/Charolette/Screen.swift
@@ -1,11 +1,23 @@
 import Foundation
 
+/// A structure representing insets of a rectangle.
+/// 
+/// Typically, insets are used to provide padding for a rectangle or bounds by which
+/// certain elements or vectors cannot reside in.
 public struct EdgeInsets: Equatable {
+    /// A constant for no insets.
     public static let zero = EdgeInsets(all: 0)
 
+    /// The number of pixels or units away from the rectangle's top edge.
     public var top: Float
+
+    /// The number of pixels or units away from the rectangle's left edge.
     public var left: Float
+
+    /// The number of pixels or units away from the rectangle's right edge.
     public var right: Float
+
+    /// The number of pixels or units away from the rectangle's bottom edge.
     public var bottom: Float
 
     public init(top: Float, left: Float, right: Float, bottom: Float) {
@@ -30,8 +42,12 @@ public struct EdgeInsets: Equatable {
     }
 }
 
+/// A structure representing information about the screen.
 public struct ScreenData {
+    /// The width and height of the screen, represented as a vector.
     public var bounds: Vector2<Float>
+
+    /// The insets set from the edges of the screen's rectangle for suitable drawing.
     public var edgeInsets: EdgeInsets
 
     public init(bounds: Vector2<Float>, insets: EdgeInsets = .zero) {
@@ -39,6 +55,9 @@ public struct ScreenData {
         self.edgeInsets = insets
     }
 
+    /// Shifts a point in space to fit within the screen's safe area.
+    /// - Parameter point: The position to shift into the safe area.
+    /// - Returns: A translated vector that is within the safe areas of the screen.
     public func fencingIn(point: Vector2<Float>) -> Vector2<Float> {
         var newPosition = point
         newPosition.x = newPosition.x.clamp(lower: edgeInsets.left, upper: bounds.x - edgeInsets.right)
