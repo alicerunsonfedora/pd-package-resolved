d6a78b8c0b0eee3add5272de7a1eee33162b1278
Marquis Kurt <software@marquiskurt.net>
2024-04-06 15:34:29 -0400

    Port Charolette Movement APIs

---
+++ Sources/Charolette/Movement.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/// A container for the movement APIs.
+public enum Movement {
+    /// Translates a movement vector according to the crank's position angle.
+    /// - Parameter position: The original movement vector to translate.
+    /// - Parameter rotation: The current angle the crank is at, if the crank is engaged.
+    /// - Parameter bounds: The bounds of the screen by which the position cannot translate past.
+    /// - Returns: A translated vector accounting for the crank's angle.
+    public static func translate(from position: Vector2<Float>,
+                                 withCrankRotation rotation: Float,
+                                 bounds: Vector2<Float>) -> Vector2<Float> {
+        // TODO: Correctly calculate the delta here to be used in the final calculations.
+        let delta = position.x - rotation
+        return Vector2<Float>(
+            x: rotation.clamp(lower: 0, upper: bounds.x),
+            y: position.y.clamp(lower: 0, upper: bounds.y))
+    }
+}
---
+++ Tests/CharoletteTests/MovementTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+import Charolette
+
+final class MovementTests: XCTestCase {
+    func testMovementTranslation() throws {
+        let originalPosition = Vector2<Float>(x: 15.0, y: 0.0)
+        let translated = Movement.translate(from: originalPosition,
+                                            withCrankRotation: 15.69,
+                                            bounds: Vector2<Float>(x: 400.0, y: 240.0))
+        XCTAssertEqual(translated, Vector2<Float>(x: 15.69, y: 0))
+    }
+}
