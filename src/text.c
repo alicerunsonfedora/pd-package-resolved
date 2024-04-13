7e5d92bac8e284586af3313e81e9249257b23bf0
Marquis Kurt <software@marquiskurt.net>
2024-04-12 23:09:28 -0400

    Nuke C source code

--- src/text.c
+++
@@ -1,6 +0,0 @@
-#include "pd_api.h"
-#include "vector.h"
-
-void drawASCIIText(PlaydateAPI *pd, const char *text, vec2i position) {
-    pd->graphics->drawText(text, strlen(text), kASCIIEncoding, position.x, position.y);
-}
